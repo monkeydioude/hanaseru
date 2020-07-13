0de3c91a4fc0e2ca1373dcdf07052f6cce03ee4f
Julien Duris <monkeydioude@gmail.com>
2020-07-13 18:00:41 +0200

    f

--- pages/kanagemu/index.tsx
+++ pages/kanagemu/index.tsx
@@ -1,28 +1,55 @@
-import LangContext from '../../services/LangContext'
-import Link from 'next/link'
+import { Component } from 'react'
 
-export default () => {
-    return (
-        <div>
+enum KanaIndex {
+    HIRAGANA,
+    KATAKANA
+}
+
+enum KanaLabel {
+    HIRAGANA = "hiragana",
+    KATAKANA = "katakana"
+}
+
+const KanaModes = new Map<number, string>([
+    [KanaIndex.HIRAGANA, KanaLabel.HIRAGANA],
+    [KanaIndex.KATAKANA, KanaLabel.KATAKANA]
+])
+
+export default class Kanagemu extends Component {
+    currentKanaMode: number
+
+    constructor(props) {
+        super(props)
+        this.currentKanaMode = KanaIndex.HIRAGANA
+    }
+
+    getCurrentKanaModeLabel() : string {
+        return KanaModes[this.currentKanaMode]
+    }
+
+    render() {
+        return (
             <div>
-                <p>Apprenez les Kana(Hiragana/Katakana) avec Kanagemu.</p>
-                <p>- Mode <u>Apprendre</u>: afficher simplement les Kana et tenter de memoriser.</p>
-                <p>- Mode <u>Entrainement</u>: petit jeu ou les Kana s'affiche de maniere aleatoire sur la grille de Kana, afin d'eviter de les apprendre dans un ordre specifique.</p>
-                <button>Ok</button>
-            </div>
                 <div>
-                <select defaultValue="mainSceneTrain" onChange={entity.switchSceneValueAttr.bind(entity)}>
-                    <option value="mainSceneTrain">Entrainez</option>
-                    <option value="mainSceneLearn">Apprenez</option>
-                </select>
-                <span> vous a ecrire les {displayKanaSelect(entity)}</span>
-                <button onClick={entity.switchSceneValueAttr.bind(entity)} value={state.getState("train") ? "train" : null}>Ok</button>
+                    <p>Apprenez les Kana(Hiragana/Katakana) avec Kanagemu.</p>
+                    <p>- Mode <u>Apprendre</u>: afficher simplement les Kana et tenter de memoriser.</p>
+                    <p>- Mode <u>Entrainement</u>: petit jeu ou les Kana s'affiche de maniere aleatoire sur la grille de Kana, afin d'eviter de les apprendre dans un ordre specifique.</p>
+                    <button>Ok</button>
+                </div>
+                <div>
+                    <select defaultValue="mainSceneTrain">
+                        <option value="mainSceneTrain">Entrainez</option>
+                        <option value="mainSceneLearn">Apprenez</option>
+                    </select>
+                    <span> vous a ecrire les {this.getCurrentKanaModeLabel()}</span>
+                    <button>Ok</button>
+                </div>
             </div>
-            </div>
-        // <LangContext.Consumer>
-        //     {({lang}) => (
-        //         <Kanagemu lang={lang}></Kanagemu>
-        //     )}
-        // </LangContext.Consumer>
-    )
+            // <LangContext.Consumer>
+            //     {({lang}) => (
+            //         <Kanagemu lang={lang}></Kanagemu>
+            //     )}
+            // </LangContext.Consumer>
+        )
+    }
 }
